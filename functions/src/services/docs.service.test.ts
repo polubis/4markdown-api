079687dfe907c047526f196987c43d0fdabb8e70
polubis <polubis@wp.pl>
2024-01-03 09:04:22 +0100

    wip

--- functions/src/services/docs.service.test.ts
+++ functions/src/services/docs.service.test.ts
@@ -1,4 +1,7 @@
+import { DocsRepository } from '../repositories/docs.repository';
 import { DocsService } from './docs.service';
+
+jest.mock(`../repositories/docs.repository`);
 
 describe(`Docs updating works when: `, () => {
   it(`throws an error when invalid name`, async () => {
@@ -10,7 +13,24 @@ describe(`Docs updating works when: `, () => {
         id: `312313`,
       });
     } catch (error) {
-      expect((error as Error).message).toBe(`Wrong name format`);
+      expect((error as Error).message).toMatchSnapshot();
+    }
+  });
+
+  it(`throws an error if docs collection is not created yet`, async () => {
+    (DocsRepository as jest.Mock).mockImplementation(() => ({
+      getMy: () => Promise.resolve({ exists: false }),
+    }));
+
+    try {
+      await DocsService.update(`3123-dsad-323`, {
+        name: `Some article valid name`,
+        code: `dasdad`,
+        visibility: `public`,
+        id: `312313`,
+      });
+    } catch (error) {
+      expect((error as Error).message).toMatchSnapshot();
     }
   });
 });
