fe0c5c69bdd4f7607828570dcde193f6875ff550
polubis <polubis@wp.pl>
2024-01-19 09:17:21 +0100

    wip

--- functions/src/core/doc.test.ts
+++ functions/src/core/doc.test.ts
@@ -12,4 +12,18 @@ describe(`Doc may be used when: `, () => {
       Doc.createPath(`31232-dasdad-3123`, `Working-with-Zustand`),
     ).toThrow();
   });
+
+  it(`throws an error if invalid description`, () => {
+    expect(() => Doc.createDescription(`31232-`)).toThrow();
+    expect(() =>
+      Doc.createDescription(
+        `31232- asdasdsada sad asd aad aad asdds a  sdsad asd asd saasda sad sadsa sadasd adsa adsadsasaddddddddddddddd asd asdsad adasas dsadsad dds asd sad asd sadsadsdsa sd dsad asasasdsa sddsa das dasdsadsadas asdsad  dddddddddddddddddddd asd asda sdasd asdasd asdad asd adsad`,
+      ),
+    ).toThrow();
+    expect(() =>
+      Doc.createDescription(
+        `31232- asdasdsada sad sadsa sadasd adsa adsadsasaddddddddddddddd asd asdsdsaada asds`,
+      ),
+    ).not.toThrow();
+  });
 });
