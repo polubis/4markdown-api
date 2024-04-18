389f6c70891891084c43177462ff9ba32d5dba00
polubis <polubis@wp.pl>
2024-04-18 19:00:42 +0200

    wip

--- functions/src/services/docs.service.ts
+++ functions/src/services/docs.service.ts
@@ -17,27 +17,33 @@ export const DocsService = {
     try {
       const allDocs = (await admin.firestore().collection(`docs`).get()).docs;
 
-      return allDocs.reduce<GetPermanentDocsDto>((acc, doc) => {
-        Object.entries(doc.data()).forEach(
-          ([id, field]: [string, DocEntityField]) => {
-            if (field.visibility === `permanent`) {
-              acc.push({
-                id,
-                cdate: field.cdate,
-                mdate: field.mdate,
-                code: field.code,
-                name: field.name,
-                visibility: field.visibility,
-                description: field.description,
-                path: field.path,
-                tags: field.tags ?? [],
-              });
-            }
-          },
-        );
+      return allDocs
+        .reduce<GetPermanentDocsDto>((acc, doc) => {
+          Object.entries(doc.data()).forEach(
+            ([id, field]: [string, DocEntityField]) => {
+              if (field.visibility === `permanent`) {
+                acc.push({
+                  id,
+                  cdate: field.cdate,
+                  mdate: field.mdate,
+                  code: field.code,
+                  name: field.name,
+                  visibility: field.visibility,
+                  description: field.description,
+                  path: field.path,
+                  tags: field.tags ?? [],
+                });
+              }
+            },
+          );
 
-        return acc;
-      }, [] as GetPermanentDocsDto);
+          return acc;
+        }, [] as GetPermanentDocsDto)
+        .sort((prev, curr) => {
+          if (prev.cdate > curr.cdate) return -1;
+          if (prev.cdate === curr.cdate) return 0;
+          return 1;
+        });
     } catch (err) {
       throw errors.internal(`Server error`);
     }
