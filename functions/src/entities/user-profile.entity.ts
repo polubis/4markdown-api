e82d359bd235bd3e2625420cb384344ef4d0ec78
polubis <polubis@wp.pl>
2024-05-11 13:47:31 +0200

    wip

---
+++ functions/src/entities/user-profile.entity.ts
@@ -0,0 +1,55 @@
+import { errors } from '../core/errors';
+import { z } from 'zod';
+
+const avatarVariantSchema = z.object({
+  h: z.number(),
+  w: z.number(),
+  src: z.string().url(),
+});
+
+const userProfileEntitySchema = z.object({
+  displayName: z
+    .string()
+    .regex(/^[a-zA-Z0-9_-]+$/)
+    .min(2)
+    .max(25)
+    .nullable(),
+  avatar: z
+    .object({
+      tn: avatarVariantSchema,
+      sm: avatarVariantSchema,
+      md: avatarVariantSchema,
+      lg: avatarVariantSchema,
+    })
+    .nullable(),
+  bio: z.string().min(60).max(300).nullable(),
+  githubUrl: z.string().url().nullable(),
+  fbUrl: z.string().url().nullable(),
+  linkedInUrl: z.string().url().nullable(),
+  blogUrl: z.string().url().nullable(),
+  twitterUrl: z.string().url().nullable(),
+  id: z.string().uuid(),
+  cdate: z.date(),
+  mdate: z.date(),
+});
+
+type UserProfileEntity = z.infer<typeof userProfileEntitySchema>;
+
+const createUserProfileEntity = (payload: unknown): UserProfileEntity => {
+  try {
+    const values = userProfileEntitySchema.parse(payload);
+    return values;
+  } catch (err) {
+    throw errors.invalidArg(`Passed payload is invalid`);
+  }
+};
+
+const isUserProfileEntity = (payload: unknown): payload is UserProfileEntity =>
+  userProfileEntitySchema.safeParse(payload).success;
+
+export {
+  createUserProfileEntity,
+  isUserProfileEntity,
+  userProfileEntitySchema,
+  UserProfileEntity,
+};
