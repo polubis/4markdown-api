39d7820bc8db8e8fd7bb563458b660d8199db87d
polubis <polubis@wp.pl>
2024-05-11 15:29:55 +0200

    wip

--- functions/src/entities/img.entity.ts
+++ functions/src/entities/img.entity.ts
@@ -1,15 +1,26 @@
 import { z } from 'zod';
 import { errors } from '../core/errors';
 
-const decode = (
-  base64: string,
-): {
-  contentType: string;
-  extension: string;
-  data: string;
-  buffer: Buffer;
-  size: number;
-} => {
+const EXTENSIONS = [`jpg`, `jpeg`, `gif`, `png`, `webp`] as const;
+const CONTENT_TYPES = [
+  `image/jpg`,
+  `image/jpeg`,
+  `image/gif`,
+  `image/png`,
+  `image/webp`,
+] as const;
+
+const schema = z.object({
+  contentType: z.enum(CONTENT_TYPES),
+  extension: z.enum(EXTENSIONS),
+  data: z.string(),
+  buffer: z.instanceof(Buffer),
+  size: z.number(),
+});
+
+type IImageEntity = z.infer<typeof schema>;
+
+const decode = (base64: string): IImageEntity => {
   const [meta] = base64.split(`,`);
   const contentType = meta.split(`:`)[1].split(`;`)[0];
   const extension = contentType.replace(`image/`, ``);
@@ -20,40 +31,13 @@ const decode = (
     (Buffer.byteLength(buffer) / 1024 / 1024).toFixed(2),
   );
 
-  return {
-    data,
-    extension,
-    contentType,
-    buffer,
-    size,
-  };
+  return schema.parse({ data, extension, contentType, buffer, size });
 };
-
-const EXTENSIONS = [`jpg`, `jpeg`, `gif`, `png`, `webp`] as const;
-const CONTENT_TYPES = [
-  `image/jpg`,
-  `image/jpeg`,
-  `image/gif`,
-  `image/png`,
-  `image/webp`,
-] as const;
 
 const ImageEntity = (image: unknown) => {
   try {
     const base64 = z.string().base64().parse(image);
-    const decodedImage = decode(base64);
-
-    const parsedImage = z
-      .object({
-        contentType: z.enum(CONTENT_TYPES),
-        extension: z.enum(EXTENSIONS),
-        data: z.string(),
-        buffer: z.number(),
-        size: z.number(),
-      })
-      .parse(decodedImage);
-
-    return parsedImage;
+    return decode(base64);
   } catch (err) {
     throw errors.invalidSchema(`ImageEntity`);
   }
