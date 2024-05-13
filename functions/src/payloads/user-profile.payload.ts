c65e45390d5b426eddc6979974413878d6930d0f
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-13 10:47:51 +0200

    wip

--- functions/src/payloads/user-profile.payload.ts
+++ functions/src/payloads/user-profile.payload.ts
@@ -3,7 +3,15 @@ import { createSchema } from '../validation/create-schema';
 import { UserProfileEntity } from '../entities/user-profile.entity';
 
 const schema = UserProfileEntity.schema
-  .omit({ cdate: true, mdate: true, id: true, avatar: true })
+  .pick({
+    bio: true,
+    displayName: true,
+    githubUrl: true,
+    blogUrl: true,
+    linkedInUrl: true,
+    fbUrl: true,
+    twitterUrl: true,
+  })
   .merge(
     z.object({
       avatar: z.union([
