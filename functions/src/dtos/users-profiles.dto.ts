7da9dbcf86587fc2bac24fb213ce45250aaad85f
Adrian Połubiński <polubis@wp.pl>
2024-05-17 10:07:13 +0200

    feat(app): add user profile management (#15)

---
+++ functions/src/dtos/users-profiles.dto.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+import { UserProfileEntity } from '../entities/user-profile.entity';
+import { createSchema } from '../validation/create-schema';
+
+const schema = UserProfileEntity.schema.pick({
+  displayName: true,
+  id: true,
+  avatar: true,
+  bio: true,
+  githubUrl: true,
+  linkedInUrl: true,
+  blogUrl: true,
+  fbUrl: true,
+  twitterUrl: true,
+});
+
+type IUserProfileDto = z.infer<typeof schema>;
+
+const UserProfileDto = createSchema(schema, `UserProfileDto`);
+
+export type { IUserProfileDto };
+export { UserProfileDto };
---
+++ functions/src/payloads/user-profile.payload.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+import { createSchema } from '../validation/create-schema';
+import { UserProfileEntity } from '../entities/user-profile.entity';
+import { base64Rgx } from '../validation/regex';
+
+const schema = UserProfileEntity.schema
+  .pick({
+    bio: true,
+    displayName: true,
+    githubUrl: true,
+    blogUrl: true,
+    linkedInUrl: true,
+    fbUrl: true,
+    twitterUrl: true,
+  })
+  .merge(
+    z.object({
+      avatar: z.union([
+        z.object({
+          type: z.literal(`noop`),
+        }),
+        z.object({
+          type: z.literal(`update`),
+          data: z.string().regex(base64Rgx),
+        }),
+        z.object({
+          type: z.literal(`remove`),
+        }),
+      ]),
+    }),
+  );
+type IUserProfilePayload = z.infer<typeof schema>;
+
+const UserProfilePayload = createSchema(schema, `UserProfilePayload`);
+
+export type { IUserProfilePayload };
+export { UserProfilePayload };
