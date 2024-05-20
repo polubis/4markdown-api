0bb460782d4a1ee748f4d607e265a03865577229
Adrian Połubiński <polubis@wp.pl>
2024-05-20 10:26:31 +0200

    feat(app): add 3 words rule for permament document name (#16)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

--- functions/src/core/doc.ts
+++ functions/src/core/doc.ts
@@ -1,5 +1,5 @@
 import { GetDocDto, GetPermanentDocsDto } from '../dtos/docs.dto';
-import { DocEntityField } from '../entities/doc.entity';
+import { DocEntityField, DocVisibility } from '../entities/doc.entity';
 import * as admin from 'firebase-admin';
 import type { Description, Name, Path, Tags } from '../entities/general';
 import { docValidators } from '../validation/doc';
@@ -69,25 +69,27 @@ export const getPermanentDocs = async (): Promise<GetPermanentDocsDto> => {
 
 const Doc = () => {};
 
-Doc.createName = (name: unknown): Name => {
+Doc.createName = (name: unknown, visibility: DocVisibility): Name => {
   if (!docValidators.name(name)) {
     throw errors.invalidArg(`Wrong name format`);
+  }
+
+  if (visibility === `permanent`) {
+    const hasAtLeast3Words = name.trim().split(` `).length >= 3;
+
+    if (!hasAtLeast3Words) {
+      throw errors.invalidArg(`At least 3 words in name are required`);
+    }
   }
 
   return name;
 };
 
-const pathsBlackList = [`/docs/`, `/doc/`];
-
-Doc.createPath = (name: Name): Path => {
-  const path = `/${Doc.createName(name)
+Doc.createPath = (name: Name, visibility: DocVisibility): Path => {
+  const path = `/${Doc.createName(name, visibility)
     .trim()
     .replace(/ /g, `-`)
     .toLowerCase()}/`;
-
-  if (pathsBlackList.includes(path)) {
-    throw errors.invalidArg(`This name is already taken`);
-  }
 
   return path;
 };
