2cb003b695d0c8f3d21f2314628ab40fdcbe77a2
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-20 13:54:50 +0200

    wip

--- functions/src/services/backups.service.ts
+++ functions/src/services/backups.service.ts
@@ -13,7 +13,7 @@ const createBackupId = (): string => {
   const minutes = String(date.getMinutes()).padStart(2, `0`);
   const seconds = String(date.getSeconds()).padStart(2, `0`);
 
-  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+  return `${day}:${month}:${year}-${hours}:${minutes}:${seconds}`;
 };
 
 const collectionIdSchema = z.enum([`docs`, `images`, `users-profiles`]);
