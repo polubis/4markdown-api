f801a6f43ae8f21e7d5a2193682af30b3bbf4f22
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-20 12:02:28 +0200

    wip

--- functions/src/services/backups.service.ts
+++ functions/src/services/backups.service.ts
@@ -1,14 +1,59 @@
-import { logger } from 'firebase-functions/v1';
-import { IBackupPayload } from '../payloads/backup.payload';
+import { BackupPayload, IBackupPayload } from '../payloads/backup.payload';
 import { errors } from '../core/errors';
+import { firestore } from 'firebase-admin';
+import { z } from 'zod';
+
+const collectionIdSchema = z.enum([`docs`, `images`, `users-profiles`]);
+
+type CollectionId = z.infer<typeof collectionIdSchema>;
+
+type DatabaseData = Record<CollectionId, Record<string, unknown>>;
+
+const getDatabase = async (
+  instance: firestore.Firestore,
+): Promise<DatabaseData> => {
+  const collections = await instance.listCollections();
+
+  const data: DatabaseData = {
+    docs: {},
+    images: {},
+    'users-profiles': {},
+  };
+
+  for (const collection of collections) {
+    const snap = await collection.get();
+
+    const collectionId = collectionIdSchema.parse(collection.id);
+
+    snap.docs.forEach((doc) => {
+      data[collectionId] = {
+        [doc.id]: doc.data(),
+      };
+    });
+  }
+
+  return data;
+};
 
 const BackupsService = {
-  create: ({ token }: IBackupPayload): Promise<void> => {
-    if (token !== process.env.BACKUP_TOKEN) {
+  create: async (payload: IBackupPayload): Promise<void> => {
+    const backupSetup = { token: process.env.BACKUP_TOKEN };
+
+    if (!BackupPayload.is(backupSetup)) {
+      throw errors.invalidArg(`Lack of token on server process`);
+    }
+
+    if (payload.token !== backupSetup.token) {
       throw errors.invalidArg(`Wrong token`);
     }
 
-    logger.info(`Backup creation started`);
+    const instance = firestore();
+
+    try {
+      const databaseData = await getDatabase(instance);
+    } catch (err) {
+      throw errors.internal(`Problem with database integrity`);
+    }
 
     return Promise.resolve();
   },
