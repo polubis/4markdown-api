93fee83253672b2b70334d6fdbed11fb75cf728f
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-21 12:05:35 +0200

    wip

---
+++ functions/src/libs/firebase-backup/defs.ts
@@ -0,0 +1,12 @@
+// const backup = Backup({
+//     source: ``,
+//     destination: ``,
+//   });
+
+//   // Get's list of available backups.
+//   backup.list();
+//   backup.create({});
+//   // Removes backup by id
+//   backup.remove({});
+//   // Using a backup/replacing data in existing database/storage.
+//   backup.use({});
