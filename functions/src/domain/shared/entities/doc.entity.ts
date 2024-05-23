0ccadc3fa5caaeccd0c6ac1c022091e08b78262e
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-23 15:13:53 +0200

    wip

--- functions/src/domain/shared/entities/doc.entity.ts
+++ functions/src/domain/shared/entities/doc.entity.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { dateRgx, docNameRgx, noEdgeSpacesRgx } from '../consts/regexes';
+import { Id } from './atoms';
 
 const DocEntityFieldBase = z.object({
   name: z.string().regex(noEdgeSpacesRgx).regex(docNameRgx).min(2).max(100),
@@ -26,7 +27,7 @@ const DocEntityField = z.union([
   PermamentDocEntityField,
 ]);
 
-const DocEntity = z.record(DocEntityField);
+const DocEntity = z.record(Id, DocEntityField);
 
 export {
   DocEntity,
