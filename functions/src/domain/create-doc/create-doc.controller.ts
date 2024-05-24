2e7973c765ec51fe085576315c588df99cdac1eb
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-24 12:57:36 +0200

    wip

--- functions/src/domain/create-doc/create-doc.controller.ts
+++ functions/src/domain/create-doc/create-doc.controller.ts
@@ -1,62 +1,16 @@
 import { Controller } from '../../libs/framework/controller';
-import { errors } from '../../libs/framework/errors';
-import { nowISO } from '../../libs/utils/date';
-import { uuid } from '../../libs/utils/uuid';
-import {
-  DocEntity,
-  PrivateDocEntityField,
-} from '../shared/entities/doc.entity';
 import { isAuthenticated } from '../shared/middleware/is-authenticated';
-import { DocsRepository } from '../shared/repositories/docs.repository';
-import { CreateDocDto } from './create-doc.dto';
 import { CreateDocPayload } from './create-doc.payload';
+import { CreateDocService } from './create-doc.service';
 import { ICreateDocDto } from './defs';
 
 const createDocController = Controller<ICreateDocDto>(
   async (context, payload) => {
     const auth = isAuthenticated(context);
-    const { name, code } = await CreateDocPayload.parseAsync(payload);
-    const id = uuid();
-    const cdate = nowISO();
-    const visibility = `private`;
-    const mdate = cdate;
-
-    const entityField = await PrivateDocEntityField.parseAsync({
-      cdate,
-      mdate,
-      visibility,
-      code,
-      name,
-    });
-
-    const [entity, dto] = await Promise.all([
-      DocEntity.parseAsync({
-        [id]: entityField,
-      }),
-      CreateDocDto.parseAsync({ ...entityField, id }),
-    ]);
-
-    const docsRepository = DocsRepository();
-    const doc = await docsRepository.getEntity(auth.uid);
-
-    if (!doc) {
-      await docsRepository.setEntity(auth.uid, entity);
-      return dto;
-    }
-
-    const hasDuplicate = Object.values(doc).some((f) => f.name === name);
-
-    if (hasDuplicate) {
-      throw errors.alreadyExists(
-        `ALREADY_EXISTS`,
-        `Document with provided name already exists`,
-      );
-    }
-
-    await docsRepository.setEntity(auth.uid, {
-      ...doc,
-      ...entity,
-    });
+    const dto = await CreateDocService.create(
+      auth.uid,
+      await CreateDocPayload.parseAsync(payload),
+    );
 
     return dto;
   },
--- functions/src/domain/create-doc/defs.ts
+++ functions/src/domain/create-doc/defs.ts
@@ -1,8 +1,13 @@
 import { z } from 'zod';
 import { CreateDocPayload } from './create-doc.payload';
 import { CreateDocDto } from './create-doc.dto';
+import { IUid } from '../shared/entities/defs';
 
 type ICreateDocPayload = z.infer<typeof CreateDocPayload>;
 type ICreateDocDto = z.infer<typeof CreateDocDto>;
 
-export type { ICreateDocPayload, ICreateDocDto };
+type ICreateDocService = {
+  create(uid: IUid, payload: ICreateDocPayload): Promise<ICreateDocDto>;
+};
+
+export type { ICreateDocPayload, ICreateDocDto, ICreateDocService };
