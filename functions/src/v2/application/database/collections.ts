570a72cc5e0b067fe2c2714fcf4bd396dfba0480
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-23 11:58:27 +0200

    wip

--- functions/src/v2/application/database/collections.ts
+++ functions/src/v2/application/database/collections.ts
@@ -3,6 +3,7 @@ import * as admin from 'firebase-admin';
 const collections = {
   documents: () => admin.firestore().collection(`docs`),
   images: () => admin.firestore().collection(`images`),
+  documentsRates: () => admin.firestore().collection(`documents-rates`),
 };
 
 export { collections };
