bd8a079843251143a59b7f0660d633d3d3ade79a
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-24 14:42:04 +0200

    wip

--- functions/src/v2/application/modules/update-document-code/update-document-code.controller.ts
+++ functions/src/v2/application/modules/update-document-code/update-document-code.controller.ts
@@ -4,9 +4,8 @@ import { z } from 'zod';
 import { validators } from '../../utils/validators';
 import { parse } from '../../../libs/framework/parse';
 import { collections } from '../../database/collections';
-import { DocumentsModel } from '../../../domain/models/document';
+import { DocumentModel, DocumentsModel } from '../../../domain/models/document';
 import { nowISO } from '../../../libs/helpers/stamps';
-import { getUserDocument } from '../../services/documents/get-user-document.service';
 
 const payloadSchema = z.object({
   id: validators.id,
@@ -17,19 +16,33 @@ const payloadSchema = z.object({
 const updateDocumentCodeController = protectedController(
   async (rawPayload, { uid }) => {
     const ref = collections.documents().doc(uid);
-    const payload = await parse(payloadSchema, rawPayload);
-    const document = await getUserDocument({ uid, documentId: payload.id });
 
-    if (!document.data) throw errors.notFound(`Document not found`);
+    const [payload, snap] = await Promise.all([
+      parse(payloadSchema, rawPayload),
+      ref.get(),
+    ]);
 
-    if (payload.mdate !== document.data.mdate)
+    if (!snap.exists) throw errors.notFound(`Documents collection not found`);
+
+    const documents = snap.data() as DocumentsModel | undefined;
+
+    if (!documents) throw errors.notFound(`Document data not found`);
+
+    const document = documents[payload.id] as DocumentModel | undefined;
+
+    if (!document) {
+      throw errors.notFound(`Document not found`);
+    }
+
+    if (payload.mdate !== document.mdate) {
       throw errors.outOfDate(`The document has been already changed`);
+    }
 
     const mdate = nowISO();
 
     await ref.update(<DocumentsModel>{
       [payload.id]: {
-        ...document.data,
+        ...document,
         code: payload.code,
         mdate,
       },
