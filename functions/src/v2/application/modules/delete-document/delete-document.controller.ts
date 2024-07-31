7e3d89461ec4787adddcc5557710bbf3802caf10
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-31 08:01:03 +0200

    wip

---
+++ functions/src/v2/application/modules/delete-document/delete-document.controller.ts
@@ -0,0 +1,41 @@
+import { protectedController } from '../../utils/controller';
+import { z } from 'zod';
+import { validators } from '../../utils/validators';
+import { parse } from '../../utils/parse';
+import { errors } from '../../utils/errors';
+import { FieldValue } from 'firebase-admin/firestore';
+
+const payloadSchema = z.object({
+  id: validators.id,
+});
+
+const deleteDocumentController = protectedController(
+  async (rawPayload, { uid, db }) => {
+    const { id: documentId } = await parse(payloadSchema, rawPayload);
+    const documentRef = db.collection(`docs`).doc(uid);
+    const documentRateRef = db.collection(`documents-rates`).doc(documentId);
+
+    return await db.runTransaction(async (transaction) => {
+      const [documentSnap, documentRateSnap] = await transaction.getAll(
+        documentRef,
+        documentRateRef,
+      );
+
+      const documentData = documentSnap.data();
+
+      if (!documentData) {
+        throw errors.notFound(`Document not found`);
+      }
+
+      documentData[documentId] = FieldValue.delete();
+
+      transaction.update(documentRef, documentData);
+
+      if (documentRateSnap) {
+        transaction.delete(documentRateRef);
+      }
+    });
+  },
+);
+
+export { deleteDocumentController };
