21bd33fdde4d9928f8caf3a1aa3dda0bbcd21d15
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-07-31 09:41:59 +0200

    wip

--- functions/src/v2/application/modules/get-permanent-documents/get-permanent-documents.controller.ts
+++ functions/src/v2/application/modules/get-permanent-documents/get-permanent-documents.controller.ts
@@ -2,6 +2,7 @@ import type {
   DocumentModel,
   PermanentDocumentModel,
 } from '../../../domain/models/document';
+import { DocumentRateModel } from '../../../domain/models/document-rate';
 import type { UserProfileModel } from '../../../domain/models/user-profile';
 import { controller } from '../../utils/controller';
 import type { Id } from '../../utils/validators';
@@ -9,18 +10,28 @@ import type { Id } from '../../utils/validators';
 type Dto = (PermanentDocumentModel & {
   id: Id;
   author: UserProfileModel | null;
+  rating: DocumentRateModel['rating'];
 })[];
 
 const getPermanentDocumentsController = controller<Dto>(async (_, { db }) => {
-  const [documentsSnap, usersProfilesSnap] = await Promise.all([
-    db.collection(`docs`).get(),
-    db.collection(`users-profiles`).get(),
-  ]);
+  const [documentsSnap, usersProfilesSnap, documentsRatesSnap] =
+    await Promise.all([
+      db.collection(`docs`).get(),
+      db.collection(`users-profiles`).get(),
+      db.collection(`documents-rates`).get(),
+    ]);
 
   const usersProfiles: Record<Id, UserProfileModel> = {};
 
   usersProfilesSnap.forEach((userSnap) => {
     usersProfiles[userSnap.id] = userSnap.data() as UserProfileModel;
+  });
+
+  const documentsRates: Record<Id, DocumentRateModel['rating']> = {};
+
+  documentsRatesSnap.forEach((documentRateSnap) => {
+    documentsRates[documentRateSnap.id] =
+      documentRateSnap.data() as DocumentRateModel['rating'];
   });
 
   const permanentDocuments: Dto = [];
@@ -34,6 +45,13 @@ const getPermanentDocumentsController = controller<Dto>(async (_, { db }) => {
           ...document,
           id: documentId,
           author: usersProfiles[documentId] ?? null,
+          rating: documentsRates[documentId] ?? {
+            ugly: 0,
+            bad: 0,
+            decent: 0,
+            good: 0,
+            perfect: 0,
+          },
         });
       }
     });
