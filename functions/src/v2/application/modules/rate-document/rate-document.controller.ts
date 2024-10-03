2e62953fbc7fd32178e7db3169acfdd938d119fe
Adrian Połubiński <polubis@wp.pl>
2024-10-03 08:26:14 +0200

    feat(app): change document votes implementation (#34)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

--- functions/src/v2/application/modules/rate-document/rate-document.controller.ts
+++ functions/src/v2/application/modules/rate-document/rate-document.controller.ts
@@ -1,4 +1,4 @@
-import { protectedController } from '../../utils/controller';
+import { controller } from '../../utils/controller';
 import { z } from 'zod';
 import { validators } from '../../utils/validators';
 import { parse } from '../../utils/parse';
@@ -7,7 +7,6 @@ import {
   DOCUMENT_RATING_CATEGORIES,
   DocumentRateModel,
 } from '../../../domain/models/document-rate';
-import { UserDocumentsVotesModel } from '../../../domain/models/user-documents-votes';
 import { createDocumentRating } from '../../utils/create-document-rating';
 
 const payloadSchema = z.object({
@@ -17,70 +16,42 @@ const payloadSchema = z.object({
 
 type Dto = DocumentRateModel['rating'];
 
-const rateDocumentController = protectedController<Dto>(
-  async (rawPayload, { uid, db }) => {
-    const { documentId, category } = await parse(payloadSchema, rawPayload);
-    const now = nowISO();
-    const documentRateRef = db.collection(`documents-rates`).doc(documentId);
-    const userDocumentsVotesRef = db
-      .collection(`users-documents-votes`)
-      .doc(uid);
+const rateDocumentController = controller<Dto>(async (rawPayload, { db }) => {
+  const { documentId, category } = await parse(payloadSchema, rawPayload);
+  const now = nowISO();
+  const documentRateRef = db.collection(`documents-rates`).doc(documentId);
 
-    return await db.runTransaction(async (transaction) => {
-      const [documentRateSnap, userDocumentsVotesSnap] =
-        await transaction.getAll(documentRateRef, userDocumentsVotesRef);
+  return await db.runTransaction(async (transaction) => {
+    const [documentRateSnap] = await transaction.getAll(documentRateRef);
 
-      const userDocumentsVotesData = userDocumentsVotesSnap.data() as
-        | UserDocumentsVotesModel
-        | undefined;
+    const documentRateData = documentRateSnap.data() as
+      | DocumentRateModel
+      | undefined;
 
-      if (userDocumentsVotesData) {
-        transaction.update(userDocumentsVotesRef, { [documentId]: category });
-      } else {
-        transaction.set(userDocumentsVotesRef, { [documentId]: category });
-      }
-
-      const documentRateData = documentRateSnap.data() as
-        | DocumentRateModel
-        | undefined;
-
-      if (!documentRateData) {
-        const model: DocumentRateModel = {
-          rating: createDocumentRating({ [category]: 1 }),
-          cdate: now,
-          mdate: now,
-        };
-
-        transaction.set(documentRateRef, model);
-
-        return model.rating;
-      }
-
-      const currentCategory = userDocumentsVotesData
-        ? userDocumentsVotesData[documentId]
-        : undefined;
-
-      if (currentCategory === category) return documentRateData.rating;
-
-      const rating: DocumentRateModel['rating'] = {
-        ...documentRateData.rating,
-        [category]: documentRateData.rating[category] + 1,
-      };
-
-      if (currentCategory && rating[currentCategory] > 0) {
-        rating[currentCategory] = rating[currentCategory] - 1;
-      }
-
-      const model: Pick<DocumentRateModel, 'mdate' | 'rating'> = {
+    if (!documentRateData) {
+      const model: DocumentRateModel = {
+        rating: createDocumentRating({ [category]: 1 }),
+        cdate: now,
         mdate: now,
-        rating,
       };
 
-      transaction.update(documentRateRef, model);
+      transaction.set(documentRateRef, model);
 
       return model.rating;
-    });
-  },
-);
+    }
+
+    const model: Pick<DocumentRateModel, 'mdate' | 'rating'> = {
+      mdate: now,
+      rating: {
+        ...documentRateData.rating,
+        [category]: documentRateData.rating[category] + 1,
+      },
+    };
+
+    transaction.update(documentRateRef, model);
+
+    return model.rating;
+  });
+});
 
 export { rateDocumentController };
