eaf9a4202cf982ea0c3b0fbde5a17b219697a741
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-03 08:22:45 +0200

    wip

--- functions/src/v2/application/modules/rate-document/rate-document.controller.ts
+++ functions/src/v2/application/modules/rate-document/rate-document.controller.ts
@@ -1,4 +1,4 @@
-import { protectedController } from '../../utils/controller';
+import { controller } from '../../utils/controller';
 import { z } from 'zod';
 import { validators } from '../../utils/validators';
 import { parse } from '../../utils/parse';
@@ -16,44 +16,42 @@ const payloadSchema = z.object({
 
 type Dto = DocumentRateModel['rating'];
 
-const rateDocumentController = protectedController<Dto>(
-  async (rawPayload, { db }) => {
-    const { documentId, category } = await parse(payloadSchema, rawPayload);
-    const now = nowISO();
-    const documentRateRef = db.collection(`documents-rates`).doc(documentId);
+const rateDocumentController = controller<Dto>(async (rawPayload, { db }) => {
+  const { documentId, category } = await parse(payloadSchema, rawPayload);
+  const now = nowISO();
+  const documentRateRef = db.collection(`documents-rates`).doc(documentId);
 
-    return await db.runTransaction(async (transaction) => {
-      const [documentRateSnap] = await transaction.getAll(documentRateRef);
+  return await db.runTransaction(async (transaction) => {
+    const [documentRateSnap] = await transaction.getAll(documentRateRef);
 
-      const documentRateData = documentRateSnap.data() as
-        | DocumentRateModel
-        | undefined;
+    const documentRateData = documentRateSnap.data() as
+      | DocumentRateModel
+      | undefined;
 
-      if (!documentRateData) {
-        const model: DocumentRateModel = {
-          rating: createDocumentRating({ [category]: 1 }),
-          cdate: now,
-          mdate: now,
-        };
-
-        transaction.set(documentRateRef, model);
-
-        return model.rating;
-      }
-
-      const model: Pick<DocumentRateModel, 'mdate' | 'rating'> = {
+    if (!documentRateData) {
+      const model: DocumentRateModel = {
+        rating: createDocumentRating({ [category]: 1 }),
+        cdate: now,
         mdate: now,
-        rating: {
-          ...documentRateData.rating,
-          [category]: documentRateData.rating[category] + 1,
-        },
       };
 
-      transaction.update(documentRateRef, model);
+      transaction.set(documentRateRef, model);
 
       return model.rating;
-    });
-  },
-);
+    }
+
+    const model: Pick<DocumentRateModel, 'mdate' | 'rating'> = {
+      mdate: now,
+      rating: {
+        ...documentRateData.rating,
+        [category]: documentRateData.rating[category] + 1,
+      },
+    };
+
+    transaction.update(documentRateRef, model);
+
+    return model.rating;
+  });
+});
 
 export { rateDocumentController };
