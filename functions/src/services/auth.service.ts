d80f6f7f7ac4ddf87b73527989b5cf084ed9d4ea
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-11 10:29:58 +0200

    wip

--- functions/src/services/auth.service.ts
+++ functions/src/services/auth.service.ts
@@ -1,11 +1,11 @@
 import { CallableContext } from 'firebase-functions/v1/https';
-import { errors } from '../core/errors';
 import { AuthData } from 'firebase-functions/lib/common/providers/https';
+import { errors } from '../v2/application/utils/errors';
 
 export const AuthService = {
   authorize: (context: CallableContext): AuthData => {
     if (!context.auth) {
-      throw errors.notAuthorized();
+      throw errors.unauthenticated();
     }
 
     return context.auth;
