38d56a4cab4f2ad219816609b6fc7767b4486b61
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-25 15:53:09 +0200

    wip

--- functions/src/core/env-checks.ts
+++
@@ -1,9 +0,0 @@
-import { IProjectId } from '../models/project-id';
-
-const isProd = (projectId: IProjectId): boolean =>
-  projectId === process.env.PROD_PROJECT_ID;
-
-const isDev = (projectId: IProjectId): boolean =>
-  projectId === process.env.DEV_PROJECT_ID;
-
-export { isDev, isProd };
