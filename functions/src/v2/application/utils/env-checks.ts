6e2d1f1d42e5c790a61f387a354143ad9fd37f56
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-29 10:26:36 +0100

    wip

--- functions/src/v2/application/utils/env-checks.ts
+++
@@ -1,7 +0,0 @@
-const isProd = (projectId: string): boolean =>
-  projectId === process.env.PROD_PROJECT_ID;
-
-const isDev = (projectId: string): boolean =>
-  projectId === process.env.DEV_PROJECT_ID;
-
-export { isProd, isDev };
