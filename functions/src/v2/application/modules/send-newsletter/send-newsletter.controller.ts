9f6058d991f8fbd86d9bd7fccdf0ffe5280dcb1e
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-29 11:52:09 +0100

    wip

--- functions/src/v2/application/modules/send-newsletter/send-newsletter.controller.ts
+++ functions/src/v2/application/modules/send-newsletter/send-newsletter.controller.ts
@@ -3,11 +3,22 @@ import { MailerSend, EmailParams, Sender, Recipient } from 'mailersend';
 import { getEmailsApiKey } from '../../utils/get-emails-api.key';
 import { getNewsletterTemplateId } from '../../utils/get-newsletter-template-id';
 import { errors } from '../../utils/errors';
+import type { Id } from '../../utils/validators';
+import type { UserProfileModel } from '../../../domain/models/user-profile';
+import type { DocumentModel } from '../../../domain/models/document';
 
 type Dto = void;
 
 // 2. URL na FE dla Unsub.
-// 5. Fix personalization
+
+type EmailArticle = {
+  url: string;
+  name: string;
+  image: string;
+  author: string;
+  description: string;
+  cdate: string;
+};
 
 const sendNewsletterController = protectedController<Dto>(
   async (_, { db, isAdmin }) => {
@@ -16,23 +27,54 @@ const sendNewsletterController = protectedController<Dto>(
     const apiKey = getEmailsApiKey();
     const templateId = getNewsletterTemplateId();
 
-    const recipients = (
-      await db.collection(`newsletter-subscribers`).listDocuments()
-    ).map(({ id: email }) => new Recipient(email));
-
     const mailersend = new MailerSend({
       apiKey,
     });
 
-    const articles = [
-      {
-        url: ``,
-        image: ``,
-        title: `XD`,
-        description: `XD`,
-        author: `XD`,
-      },
-    ];
+    const [subscribersSnap, documentsSnap, usersProfilesSnap] =
+      await Promise.all([
+        db.collection(`newsletter-subscribers`).listDocuments(),
+        db.collection(`docs`).get(),
+        db.collection(`users-profiles`).get(),
+      ]);
+
+    const recipients = subscribersSnap.map(
+      ({ id: email }) => new Recipient(email),
+    );
+
+    const usersProfiles: Record<Id, UserProfileModel> = {};
+
+    usersProfilesSnap.docs.forEach((userSnap) => {
+      usersProfiles[userSnap.id] = userSnap.data() as UserProfileModel;
+    });
+
+    const articles: EmailArticle[] = [];
+
+    documentsSnap.docs.forEach((documentsListSnap) => {
+      const userId = documentsListSnap.id;
+      const documentsListData = Object.entries(documentsListSnap.data());
+
+      documentsListData.forEach(([, document]: [Id, DocumentModel]) => {
+        if (document.visibility === `permanent`) {
+          articles.push({
+            url: `https://4markdown.com${document.path}`,
+            name: document.name,
+            description: document.description,
+            image: document.visibility,
+            author: usersProfiles[userId].displayName ?? `Anonymous`,
+            cdate: document.cdate,
+          });
+        }
+      });
+    });
+
+    articles
+      .sort((prev, curr) => {
+        if (prev.cdate > curr.cdate) return -1;
+        if (prev.cdate === curr.cdate) return 0;
+        return 1;
+      })
+      .splice(0, 2);
 
     const emailParams = new EmailParams()
       .setFrom(new Sender(`newsletter@4markdown.com`, `4markdown`))
