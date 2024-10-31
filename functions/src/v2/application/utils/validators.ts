5af2d8c03dc5b6db8bb0c01d863ad474145dd43d
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-10-31 14:50:41 +0100

    wip

--- functions/src/v2/application/utils/validators.ts
+++ functions/src/v2/application/utils/validators.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { regexes } from './regexes';
+import { createDocumentPath } from './create-document-path';
 
 const validators = {
   id: z.string().min(1),
@@ -15,12 +16,25 @@ const validators = {
   },
 };
 
+const documentName = z
+  .string()
+  .transform((name) => name.trim())
+  .transform((name) => ({
+    path: createDocumentPath(name),
+    name,
+  }))
+  .refine(
+    ({ path }) => path.length >= 3 && path.length <= 15,
+    `Generated path from document name must be between 3-15`,
+  );
+
 const email = z.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
 
 type Id = z.infer<typeof validators.id>;
 type Date = z.infer<typeof validators.date>;
 type Email = z.infer<typeof email>;
 type Url = string;
+type DocumentName = z.infer<typeof documentName>;
 
-export type { Id, Date, Email, Url };
+export type { Id, Date, Email, Url, DocumentName };
 export { validators, email };
