270d0337c0aa9b6b6bcc993d68adff176d94ec59
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-11-04 10:34:06 +0100

    wip

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
     '^@controllers/(.*)$': `<rootDir>/functions/src/v2/application/modules/controllers/$1`,
+    '^@database/(.*)$': `<rootDir>/functions/src/v2/application/database/$1`,
     '^@domain/(.*)$': `<rootDir>/functions/src/v2/domain/$1`,
     '^@libs/(.*)$': `<rootDir>/functions/src/v2/libs/$1`,
     '^@models/(.*)$': `<rootDir>/functions/src/v2/domain/models/$1`,
