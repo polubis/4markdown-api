1754e440369ff136401e229abe1509f8644cb332
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-16 13:58:41 +0100

    wip

--- functions/src/v2/application/modules/get-accessible-document/get-accessible-document.controller.ts
+++ functions/src/v2/application/modules/get-accessible-document/get-accessible-document.controller.ts
@@ -9,8 +9,8 @@ import type {
 } from '@domain/models/document';
 import { errors } from '@utils/errors';
 import type { UserProfileModel } from '@domain/models/user-profile';
-import type { DocumentRateModel } from '@domain/models/document-rate';
-import { createDocumentRating } from '@utils/create-document-rating';
+import type { RateModel } from '@domain/models/rate';
+import { createRating } from '@utils/create-rating';
 
 const payloadSchema = z.object({
   id,
@@ -18,7 +18,7 @@ const payloadSchema = z.object({
 
 type SharedDtoPart = {
   author: UserProfileModel | null;
-  rating: DocumentRateModel['rating'];
+  rating: RateModel['rating'];
 };
 
 type Dto =
@@ -67,13 +67,13 @@ const getAccessibleDocumentController = controller<Dto>(
       | UserProfileModel
       | undefined;
     const documentRate = documentRateSnap.data() as
-      | DocumentRateModel
+      | RateModel
       | undefined;
 
     const foundDocument = foundDocumentEntry.document;
     const author = userProfile ?? null;
-    const defaultRating = createDocumentRating();
-    const rating: DocumentRateModel['rating'] =
+    const defaultRating = createRating();
+    const rating: RateModel['rating'] =
       documentRate?.rating ?? defaultRating;
 
     if (foundDocument.visibility === `permanent`) {
