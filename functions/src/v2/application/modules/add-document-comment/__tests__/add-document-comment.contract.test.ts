718a4b7cb7c53190227c805b3fb30f29f409c49d
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-16 14:06:53 +0100

    wip

---
+++ functions/src/v2/application/modules/add-document-comment/__tests__/add-document-comment.contract.test.ts
@@ -0,0 +1,39 @@
+import { addDocumentCommentPayloadSchema } from '../add-document-comment.contract';
+
+describe(`Add document comment contract works when`, () => {
+  it(`accepts valid payload`, () => {
+    expect(() =>
+      addDocumentCommentPayloadSchema.parse({
+        documentId: `some-string-id`,
+        content: `a`,
+      }),
+    ).not.toThrow();
+  });
+
+  it(`rejects invalid payload`, () => {
+    expect(() =>
+      addDocumentCommentPayloadSchema.parse({
+        documentId: null,
+      }),
+    ).toThrow();
+    expect(() =>
+      addDocumentCommentPayloadSchema.parse({
+        documentId: `some-string-id`,
+      }),
+    ).toThrow();
+    expect(() =>
+      addDocumentCommentPayloadSchema.parse({
+        documentId: `some-string-id`,
+        content: ``,
+      }),
+    ).toThrow();
+    expect(() =>
+      addDocumentCommentPayloadSchema.parse({
+        documentId: `some-string-id`,
+        content: null,
+      }),
+    ).toThrow();
+    expect(() => addDocumentCommentPayloadSchema.parse({})).toThrow();
+    expect(() => addDocumentCommentPayloadSchema.parse(null)).toThrow();
+  });
+});
