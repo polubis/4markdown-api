4e1c5502f6d88cf97c91cf30edfe009f64f66464
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-17 11:40:59 +0100

    wip

---
+++ functions/src/v2/application/modules/edit-document-comment/__test__/edit-document-comment.test.ts
@@ -0,0 +1,40 @@
+import { editDocumentCommentPayloadSchema } from '../edit-document-comment.contract';
+
+describe(`Edit document comment contract`, () => {
+  it(`accepts a valid payload`, () => {
+    const validPayload = {
+      document: { id: `some-id`, authorId: `some-id` },
+      comment: {
+        id: `some-id`,
+        content: `a`,
+        authorId: `some-id`,
+        mdate: `2024-06-17T12:34:56.789Z`,
+      },
+    };
+
+    expect(() =>
+      editDocumentCommentPayloadSchema.parse(validPayload),
+    ).not.toThrow();
+  });
+
+  const invalidPayloads = [
+    { description: `when required fields are missing`, payload: {} },
+    { description: `when data is null`, payload: null },
+    {
+      description: `when fields are invalid`,
+      payload: { document: { id: null } },
+    },
+    {
+      description: `when fields are incomplete`,
+      payload: { document: { id: `some-id` } },
+    },
+    {
+      description: `when fields are empty`,
+      payload: { document: {}, content: `` },
+    },
+  ];
+
+  test.each(invalidPayloads)(`rejects the payload %s`, ({ payload }) => {
+    expect(() => editDocumentCommentPayloadSchema.parse(payload)).toThrow();
+  });
+});
