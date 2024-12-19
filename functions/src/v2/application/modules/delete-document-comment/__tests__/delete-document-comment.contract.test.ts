990982f6fd171537a29ec713b6e19afbb1f1ebf9
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-19 07:52:30 +0100

    wip

---
+++ functions/src/v2/application/modules/delete-document-comment/__tests__/delete-document-comment.contract.test.ts
@@ -0,0 +1,58 @@
+import { deleteDocumentCommentPayloadSchema } from '../delete-document-comment.contract';
+
+describe(`Delete document comment contract works when`, () => {
+  it(`it accepts a valid payload`, () => {
+    const validPayload = {
+      comment: {
+        id: `some-string-id`,
+        mdate: `2024-06-17T12:34:56.789Z`,
+      },
+    };
+
+    expect(() =>
+      deleteDocumentCommentPayloadSchema.parse(validPayload),
+    ).not.toThrow();
+  });
+
+  const invalidPayloads = [
+    { description: `required fields are missing`, payload: {} },
+    { description: `the data is null`, payload: null },
+    {
+      description: `some fields are incomplete`,
+      payload: {
+        comment: { ids: `some-string-id`, mdate: `2024-06-17T12:34:56.789Z` },
+      },
+    },
+    {
+      description: `a field has invalid data`,
+      payload: {
+        comment: {
+          id: null,
+          mdate: `2024-06-17T12:34:56.789Z`,
+        },
+      },
+    },
+    {
+      description: `unexpected fields are included`,
+      payload: {
+        comment: {
+          id: `some-string-id`,
+          mdate: ``,
+        },
+      },
+    },
+    {
+      description: `fields contain invalid values`,
+      payload: {
+        comment: {
+          id: null,
+          mdate: ``,
+        },
+      },
+    },
+  ];
+
+  test.each(invalidPayloads)(`it rejects the payload if %s`, ({ payload }) => {
+    expect(() => deleteDocumentCommentPayloadSchema.parse(payload)).toThrow();
+  });
+});
