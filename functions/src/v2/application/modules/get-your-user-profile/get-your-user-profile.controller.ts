d89a25473d561bdfd8cafc1d1be300024a0e1d99
Adrian Połubiński <polubis@wp.pl>
2025-01-20 16:25:35 +0100

    rewrite(app): get your user profile endpoint (#61)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

---
+++ functions/src/v2/application/modules/get-your-user-profile/get-your-user-profile.controller.ts
@@ -0,0 +1,18 @@
+import { protectedController } from '@utils/controller';
+import { parse } from '@utils/parse';
+import {
+  type GetYourUserProfileDto,
+  getYourUserProfilePayloadSchema,
+} from './get-your-user-profile.contract';
+import { getYourUserProfileHandler } from './get-your-user-profile.handler';
+
+const getYourUserProfileController = protectedController<GetYourUserProfileDto>(
+  async (rawPayload, context) => {
+    return await getYourUserProfileHandler({
+      context,
+      payload: await parse(getYourUserProfilePayloadSchema, rawPayload),
+    });
+  },
+);
+
+export { getYourUserProfileController };
