9602785591304f8f4eb7da810c3466954da3f1c3
Adrian Połubiński <polubis@wp.pl>
2025-01-23 14:06:06 +0100

    rewrite(app): update your user profile (#62)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

--- functions/src/v2/application/utils/validators.ts
+++ functions/src/v2/application/utils/validators.ts
@@ -1,16 +1,24 @@
 import { z } from 'zod';
 import { regexes } from './regexes';
 
-const id = z.string().min(1);
-const email = z.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
-const base64 = z.string().min(1).regex(regexes.base64);
-const date = z.string().regex(regexes.date);
+const id = z.string().trim().min(1);
+const email = z
+  .string()
+  .trim()
+  .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+const base64 = (message: string) =>
+  z.string().trim().min(1, message).regex(regexes.base64, message);
+const date = z.string().trim().regex(regexes.date);
+const url = (message: string) => z.string().trim().url(message);
+const text = z.string().trim();
 
 type Id = z.infer<typeof id>;
 type Date = z.infer<typeof date>;
 type Email = z.infer<typeof email>;
-type Url = string;
-type Base64 = z.infer<typeof base64>;
+type Url = z.infer<ReturnType<typeof url>>;
+type Base64 = z.infer<ReturnType<typeof base64>>;
+type Text = z.infer<typeof text>;
+type Slug = string;
 
-export type { Id, Date, Email, Url, Base64 };
-export { id, date, email, base64 };
+export type { Id, Date, Email, Url, Base64, Text, Slug };
+export { id, date, email, base64, url, text };
