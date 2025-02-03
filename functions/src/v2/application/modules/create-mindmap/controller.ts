5871214b97f2ead2249baf6c63728a028fab0756
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-03 09:00:23 +0100

    wip

---
+++ functions/src/v2/application/modules/create-mindmap/controller.ts
@@ -0,0 +1,100 @@
+import { MindmapId, Visibility } from '@domain/atoms/general';
+import { MindmapDto } from '@domain/models/mindmap';
+import { nowISO, uuid } from '@libs/helpers/stamps';
+import { protectedController } from '@utils/controller';
+import { createSlug } from '@utils/create-slug';
+import { errors } from '@utils/errors';
+import { z } from 'zod';
+
+const schema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .transform((name) => {
+        const slug = createSlug(name);
+
+        return {
+          raw: name,
+          path: `/${slug}/`,
+          slug,
+          segments: slug === `` ? [] : slug.split(`-`),
+        };
+      })
+      .superRefine(({ segments, raw }, { addIssue }) => {
+        if (!(raw.length >= 1 && raw.length <= 70)) {
+          addIssue({
+            code: `custom`,
+            message: `Mindmap name must be between 1-70 characters`,
+          });
+        }
+
+        if (!(segments.length >= 1 && segments.length <= 15)) {
+          addIssue({
+            code: `custom`,
+            message: `Generated path from mindmap name must be between 1-15`,
+          });
+        }
+      }),
+    description: z
+      .string()
+      .trim()
+      .min(110, `Description must be at least 110 characters`)
+      .max(160, `Description must be fewer than 160 characters`),
+  })
+  .brand(`payload`);
+
+type Payload = z.infer<typeof schema>;
+
+type Dto = MindmapDto;
+
+const validate = async (rawPayload: unknown): Promise<Payload> => {
+  try {
+    return await schema.parseAsync(rawPayload);
+  } catch (error: unknown) {
+    throw errors.schema(error);
+  }
+};
+
+const createMindmapController = protectedController<Dto>(
+  async (rawPayload, context) => {
+    const payload = await validate(rawPayload);
+    const userMindmapsRef = context.db
+      .collection(`user-mindmaps`)
+      .doc(context.uid);
+
+    const hasDuplicateSnapshot = await userMindmapsRef
+      .collection(`mindmaps`)
+      .where(`path`, `==`, payload.name.path)
+      .count()
+      .get();
+
+    const hasDuplicate = hasDuplicateSnapshot.data().count > 0;
+
+    if (!hasDuplicate) {
+      throw errors.exists(`Mindmap with ${payload.name.raw} already exists`);
+    }
+
+    const mindmapId = uuid() as MindmapId;
+    const now = nowISO();
+
+    const newMindmap: MindmapDto = {
+      id: mindmapId,
+      cdate: now,
+      mdate: now,
+      name: payload.name.raw,
+      path: payload.name.path,
+      description: payload.description,
+      edges: [],
+      nodes: [],
+      visibility: Visibility.Private,
+      orientation: `y`,
+    };
+
+    await userMindmapsRef.collection(`mindmaps`).add(newMindmap);
+
+    return newMindmap;
+  },
+);
+
+export { createMindmapController };
