f89fd0908f6d0decd4cc1662933cb7a3572f078c
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-24 13:59:11 +0100

    wip

---
+++ functions/src/v2/application/utils/mindmap-schemas.ts
@@ -0,0 +1,58 @@
+import { z } from 'zod';
+import {
+  clientGeneratedId,
+  cords,
+  description,
+  name,
+  text,
+  url,
+} from './validators';
+import { MINDMAP_EDGE_TYPES, MINDMAP_NODE_TYPES } from '@domain/models/mindmap';
+
+const mindmapEdges = () => {
+  const id = clientGeneratedId();
+
+  return z.array(
+    z.object({
+      id,
+      source: id,
+      target: id,
+      type: z.enum(MINDMAP_EDGE_TYPES),
+    }),
+  );
+};
+
+const mindmapNodes = () => {
+  const [externalType, embeddedType] = MINDMAP_NODE_TYPES;
+  const id = clientGeneratedId();
+  const position = cords();
+  const nodeDescription = description().nullable();
+  const nodeName = name();
+
+  return z.array(
+    z.union([
+      z.object({
+        id,
+        type: z.literal(externalType),
+        position,
+        data: z.object({
+          url: url(`Wrong url format in node`),
+          description: nodeDescription,
+          name: nodeName,
+        }),
+      }),
+      z.object({
+        id,
+        type: z.literal(embeddedType),
+        position,
+        data: z.object({
+          content: text.nullable(),
+          description: nodeDescription,
+          name: nodeName,
+        }),
+      }),
+    ]),
+  );
+};
+
+export { mindmapEdges, mindmapNodes };
