093dd6ecec6068e6aca0a37b284ba67da448c1cc
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-10 15:53:31 +0100

    wip

---
+++ functions/src/v2/application/modules/update-mindmap/index.ts
@@ -0,0 +1,65 @@
+import { MindmapModel } from '@domain/models/mindmap';
+import { nowISO } from '@libs/helpers/stamps';
+import { protectedController } from '@utils/controller';
+import { errors } from '@utils/errors';
+import { parse } from '@utils/parse';
+import { date, description, id, name, tags } from '@utils/validators';
+import { z } from 'zod';
+
+const payloadSchema = z.object({
+  id,
+  mdate: date,
+  name: name(),
+  description: description().nullable(),
+  tags: tags().nullable(),
+});
+
+type Dto = Pick<
+  MindmapModel,
+  'mdate' | 'name' | 'description' | 'tags' | 'path'
+>;
+
+const updateMindmapController = protectedController<Dto>(
+  async (rawPayload, { uid, db }) => {
+    const payload = await parse(payloadSchema, rawPayload);
+    const yourMindmapsRef = db.collection(`user-mindmaps`).doc(uid);
+    const yourMindmapRef = yourMindmapsRef
+      .collection(`mindmaps`)
+      .doc(payload.id);
+
+    const yourMindmapData = (await yourMindmapRef.get()).data() as
+      | MindmapModel
+      | undefined;
+
+    if (!yourMindmapData) {
+      throw errors.notFound(`Cannot find mindmap`);
+    }
+
+    if (yourMindmapData.mdate !== payload.mdate) {
+      throw errors.outOfDate(`Cannot remove already changed mindmap`);
+    }
+
+    const updateMindmapData: Pick<
+      MindmapModel,
+      'mdate' | 'name' | 'description' | 'tags' | 'path'
+    > = {
+      name: payload.name.raw,
+      path: payload.name.path,
+      description: payload.description,
+      tags: payload.tags,
+      mdate: nowISO(),
+    };
+
+    await yourMindmapRef.update(updateMindmapData);
+
+    return {
+      mdate: updateMindmapData.mdate,
+      name: updateMindmapData.name,
+      path: updateMindmapData.path,
+      tags: updateMindmapData.tags,
+      description: updateMindmapData.description,
+    };
+  },
+);
+
+export { updateMindmapController };
