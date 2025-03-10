8b67866e4b65542319ed7cd864dacebcaa755b0a
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-10 15:15:09 +0100

    wip

--- functions/src/v2/application/modules/delete-mindmap/index.ts
+++ functions/src/v2/application/modules/delete-mindmap/index.ts
@@ -1,28 +1,54 @@
+import { MindmapModel } from '@domain/models/mindmap';
 import { protectedController } from '@utils/controller';
-import { id } from '@utils/validators';
+import { errors } from '@utils/errors';
+import { parse } from '@utils/parse';
+import { date, id } from '@utils/validators';
 import { FieldValue } from 'firebase-admin/firestore';
+import { HttpsError } from 'firebase-functions/https';
 import { z } from 'zod';
 
-const payloadSchema = z
-  .object({
-    id,
-  })
-  .strict();
+const payloadSchema = z.object({
+  id,
+  mdate: date,
+});
 
 const deleteMindmapController = protectedController<null>(
   async (rawPayload, { uid, db }) => {
-    const payload = await payloadSchema.parseAsync(rawPayload);
+    const payload = await parse(payloadSchema, rawPayload);
     const yourMindmapsRef = db.collection(`user-mindmaps`).doc(uid);
     const yourMindmapRef = yourMindmapsRef
       .collection(`mindmaps`)
       .doc(payload.id);
 
-    await db.runTransaction(async (t) => {
+    const result = await db.runTransaction<
+      { is: `fail`; error: HttpsError } | { is: `ok` }
+    >(async (t) => {
+      const yourMindmapData = (await t.get(yourMindmapRef)).data() as
+        | MindmapModel
+        | undefined;
+
+      if (!yourMindmapData) {
+        return { is: `fail`, error: errors.notFound(`Cannot find mindmap`) };
+      }
+
+      if (yourMindmapData.mdate !== payload.mdate) {
+        return {
+          is: `fail`,
+          error: errors.outOfDate(`Cannot remove already changed mindmap`),
+        };
+      }
+
       t.delete(yourMindmapRef);
       t.update(yourMindmapsRef, {
         mindmapsCount: FieldValue.increment(-1),
       });
+
+      return { is: `ok` };
     });
+
+    if (result.is === `fail`) {
+      throw result.error;
+    }
 
     return null;
   },
