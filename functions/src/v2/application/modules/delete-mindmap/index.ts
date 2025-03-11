2cf6f147067c804a0a0fd10835b7671f86f75862
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-11 12:36:49 +0100

    wip

--- functions/src/v2/application/modules/delete-mindmap/index.ts
+++ functions/src/v2/application/modules/delete-mindmap/index.ts
@@ -1,17 +1,16 @@
-import { mindmapIdSchema, MindmapModel } from '@domain/models/mindmap';
+import { mindmapIdSchema } from '@domain/models/mindmap';
 import { protectedController } from '@utils/controller';
-import { errors } from '@utils/errors';
 import { parse } from '@utils/parse';
-import { date } from '@utils/validators';
 import { FieldValue } from 'firebase-admin/firestore';
 import { z } from 'zod';
 
 const payloadSchema = z.object({
   id: mindmapIdSchema,
-  mdate: date,
 });
 
-const deleteMindmapController = protectedController<null>(
+type Dto = null;
+
+const deleteMindmapController = protectedController<Dto>(
   async (rawPayload, { uid, db }) => {
     const payload = await parse(payloadSchema, rawPayload);
     const yourMindmapsRef = db.collection(`user-mindmaps`).doc(uid);
@@ -20,18 +19,6 @@ const deleteMindmapController = protectedController<null>(
       .doc(payload.id);
 
     return db.runTransaction(async (t) => {
-      const yourMindmapData = (await t.get(yourMindmapRef)).data() as
-        | MindmapModel
-        | undefined;
-
-      if (!yourMindmapData) {
-        throw errors.notFound(`Cannot find mindmap`);
-      }
-
-      if (yourMindmapData.mdate !== payload.mdate) {
-        throw errors.outOfDate(`Cannot remove already changed mindmap`);
-      }
-
       t.delete(yourMindmapRef);
       t.update(yourMindmapsRef, {
         mindmapsCount: FieldValue.increment(-1),
