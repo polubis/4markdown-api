80ad82be8a5389f160a928513509c331429c5167
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-19 10:10:30 +0100

    wip

--- functions/src/v2/application/modules/get-permanent-mindmaps/index.ts
+++ functions/src/v2/application/modules/get-permanent-mindmaps/index.ts
@@ -1,33 +1,21 @@
+import { Visibility } from '@domain/atoms/general';
 import { type MindmapModel } from '@domain/models/mindmap';
 import { controller } from '@utils/controller';
-import { parse } from '@utils/parse';
-import { id, type Id } from '@utils/validators';
-import { z } from 'zod';
-
-const payloadSchema = z.object({
-  limit: z.number().min(10).max(100),
-  lastMindmapId: id.optional(),
-});
+import { type Id } from '@utils/validators';
 
 type Dto = (MindmapModel & { id: Id })[];
 
-const getPermanentMindmapsController = controller<Dto>(
-  async (rawPayload, { db }) => {
-    const payload = await parse(payloadSchema, rawPayload);
+const getPermanentMindmapsController = controller<Dto>(async (_, { db }) => {
+  const mindmapsQuery = db
+    .collectionGroup(`mindmaps`)
+    .where(`visibility`, `==`, Visibility.Permanent);
 
-    const mindmapsQuery = db
-      .collectionGroup(`mindmaps`)
-      .where(`isPermanent`, `==`, true)
-      .orderBy(`cdate`, `desc`)
-      .limit(payload.limit);
+  const mindmapsSnap = await mindmapsQuery.get();
 
-    const mindmapsSnap = await mindmapsQuery.get();
-
-    return mindmapsSnap.docs.map((doc) => ({
-      ...(doc.data() as MindmapModel),
-      id: doc.id,
-    }));
-  },
-);
+  return mindmapsSnap.docs.map((doc) => ({
+    ...(doc.data() as MindmapModel),
+    id: doc.id,
+  }));
+});
 
 export { getPermanentMindmapsController };
