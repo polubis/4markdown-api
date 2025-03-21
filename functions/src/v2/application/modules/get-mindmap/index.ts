fc57130c673d7840c67c90f251b4ed61ffe76975
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-03-21 09:35:52 +0100

    wip

--- functions/src/v2/application/modules/get-mindmap/index.ts
+++ functions/src/v2/application/modules/get-mindmap/index.ts
@@ -1,5 +1,7 @@
 import { Visibility } from '@domain/atoms/general';
+import { AccountPermissionModel } from '@domain/models/account-permission';
 import { type MindmapModel } from '@domain/models/mindmap';
+import { type UserProfileModel } from '@domain/models/user-profile';
 import { controller } from '@utils/controller';
 import { errors } from '@utils/errors';
 import { parse } from '@utils/parse';
@@ -11,17 +13,27 @@ const payloadSchema = z.object({
   authorId: id,
 });
 
-type Dto = MindmapModel & { id: Id; authorId: Id };
+type Dto = MindmapModel & {
+  id: Id;
+  authorId: Id;
+  authorProfile: UserProfileModel | null;
+  isAuthorTrusted: boolean;
+};
 
 const getMindmapController = controller<Dto>(async (rawPayload, { db }) => {
   const { mindmapId, authorId } = await parse(payloadSchema, rawPayload);
 
-  const mindmapSnap = await db
-    .collection(`user-mindmaps`)
-    .doc(authorId)
-    .collection(`mindmaps`)
-    .doc(mindmapId)
-    .get();
+  const [mindmapSnap, authorProfileSnap, accountPermissionsSnap] =
+    await Promise.all([
+      db
+        .collection(`user-mindmaps`)
+        .doc(authorId)
+        .collection(`mindmaps`)
+        .doc(mindmapId)
+        .get(),
+      db.collection(`users-profiles`).doc(authorId).get(),
+      db.collection(`account-permissions`).doc(authorId).get(),
+    ]);
 
   const mindmap = mindmapSnap.data() as MindmapModel | undefined;
 
@@ -29,10 +41,20 @@ const getMindmapController = controller<Dto>(async (rawPayload, { db }) => {
     throw errors.notFound(`Mindmap not found`);
   }
 
+  const accountPermissions = accountPermissionsSnap.data() as
+    | AccountPermissionModel
+    | undefined;
+
+  const authorProfile = authorProfileSnap.data() as
+    | UserProfileModel
+    | undefined;
+
   return {
     ...mindmap,
     id: mindmapId,
     authorId,
+    authorProfile: authorProfile ?? null,
+    isAuthorTrusted: accountPermissions?.trusted ?? false,
   };
 });
 
