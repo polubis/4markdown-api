928d67d3bba4eff9e82e8bb6d9ee9e9aeef9e3c4
polubis <polubis@wp.pl>
2025-04-05 14:41:20 +0200

    wip

--- functions/src/v2/application/utils/validate-markdown.ts
+++ functions/src/v2/application/utils/validate-markdown.ts
@@ -1,52 +1,54 @@
 import { type Markdown } from './validators';
-// import { unified } from 'unified';
-// import remarkParse from 'remark-parse';
-// import remarkGfm from 'remark-gfm';
-// import remarkMath from 'remark-math';
-// import remarkRehype from 'remark-rehype';
-// import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
-// import rehypeStringify from 'rehype-stringify';
-// import { errors } from './errors';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import remarkRehype from 'remark-rehype';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
+import rehypeStringify from 'rehype-stringify';
+import { errors } from './errors';
 
-// const sanitizeSchema = {
-//   ...defaultSchema,
-// };
+const sanitizeSchema = {
+  ...defaultSchema,
+};
 
 const validateMarkdown = async (markdown: Markdown): Promise<void> => {
-  console.log(markdown);
-  // if (markdown === ``) return;
-  // let originalHtml = ``;
-  // let sanitizedHtml = ``;
-  // try {
-  //   const processorOriginal = unified()
-  //     .use(remarkParse)
-  //     .use(remarkGfm)
-  //     .use(remarkMath)
-  //     .use(remarkRehype, { allowDangerousHtml: true })
-  //     .use(rehypeStringify);
-  //   const processorSanitized = unified()
-  //     .use(remarkParse)
-  //     .use(remarkGfm)
-  //     .use(remarkMath)
-  //     .use(remarkRehype, { allowDangerousHtml: true })
-  //     .use(rehypeSanitize, sanitizeSchema)
-  //     .use(rehypeStringify);
-  //   const [original, sanitized] = await Promise.all([
-  //     processorOriginal.process(markdown),
-  //     processorSanitized.process(markdown),
-  //   ]);
-  //   originalHtml = original.toString();
-  //   sanitizedHtml = sanitized.toString();
-  // } catch (e) {
-  //   throw errors.internal(
-  //     `Something went wrong with sanitization. Please try again later`,
-  //   );
-  // }
-  // if (originalHtml !== sanitizedHtml) {
-  //   throw errors.badRequest(
-  //     `Unsafe content detected. If you will continue these attempts the system will automatically block you`,
-  //   );
-  // }
+  if (markdown === ``) return;
+
+  let [originalHtml, sanitizedHtml] = [``, ``];
+
+  try {
+    const processorOriginal = unified()
+      .use(remarkParse)
+      .use(remarkGfm)
+      .use(remarkMath)
+      .use(remarkRehype, { allowDangerousHtml: true })
+      .use(rehypeStringify);
+    const processorSanitized = unified()
+      .use(remarkParse)
+      .use(remarkGfm)
+      .use(remarkMath)
+      .use(remarkRehype, { allowDangerousHtml: true })
+      .use(rehypeSanitize, sanitizeSchema)
+      .use(rehypeStringify);
+
+    const [original, sanitized] = await Promise.all([
+      processorOriginal.process(markdown),
+      processorSanitized.process(markdown),
+    ]);
+
+    originalHtml = original.toString();
+    sanitizedHtml = sanitized.toString();
+  } catch (e) {
+    throw errors.internal(
+      `Something went wrong with sanitization. Please try again later`,
+    );
+  }
+  if (originalHtml !== sanitizedHtml) {
+    throw errors.badRequest(
+      `Unsafe content detected. If you will continue these attempts the system will automatically block you`,
+    );
+  }
 };
 
 export { validateMarkdown };
